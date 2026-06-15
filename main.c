3eddf3a8bd968f1afdd371c2bb2a95057ff19005
shikigami <mazenkassem101@gmail.com>
2026-06-15 18:50:35 +0300

    feat: add built-in functions
    add the following main built-in functions:
    - cd
    - help
    - exit

--- main.c
+++ main.c
@@ -8,6 +8,57 @@
 #define ASH_TOK_BUFSIZE 64
 #define ASH_TOK_DELIM " \t\r\n\a"
 
+// List of builtin commands, followed by their corresponding functions.
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+    &ash_cd,
+    &ash_help,
+    &ash_exit
+};
+
+int ash_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+// Builtin function implementations.
+
+int ash_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "ash: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("ash");
+        }
+    }
+    return 1;
+}
+
+int ash_help(char **args)
+{
+    int i;
+    printf("Shikigami's ash\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < ash_num_builtins(); i++) {
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int ash_exit(char **args){
+    return 0;
+}
+
 int ash_launch(char** args){
     pid_t pid, wpid;
     int status;
